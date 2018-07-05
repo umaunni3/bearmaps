fc30ea83f23b3430842cbe8559f2d17c6fa51000
Uma Unni <umaunni@berkeley.edu>
2018-07-05 15:26:25 -0700

    updated UnionFind to address bugs?

--- lab11/UnionFind.java
+++ lab11/UnionFind.java
@@ -1,12 +1,10 @@
 public class UnionFind {
 
-    /* TODO: Add instance variables here. */
     public int[] vertices;
 
     /* Creates a UnionFind data structure holding N vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int N) {
-        // TODO: YOUR CODE HERE
         vertices = new int[N];
         for (int i = 0; i < N; i++) {
             vertices[i] = -1;
@@ -15,26 +13,22 @@ public class UnionFind {
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
-        // TODO: YOUR CODE HERE
         int x = v;
         while (vertices[x] >= 0) {
             x = vertices[x];
         }
-        return vertices[x];
+        return -1 * vertices[x];
 
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
-        // TODO: YOUR CODE HERE
-
         return vertices[v];
     }
 
     /* Returns true if nodes V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO: YOUR CODE HERE
         int v1P = parent(v1);
         int v2P = parent(v2);
         if ((v1P < 0 && v2P == v1) || (v1P < 0 && v1P == v2)) {
@@ -52,14 +46,17 @@ public class UnionFind {
        allowing for fast search-time. If invalid vertices are passed into this
        function, throw an IllegalArgumentException. */
     public int find(int v) {
-        // TODO: YOUR CODE HERE
-        if (vertices[v] < 0) {
-            // this node is the root!
-            return v;
-        } else {
-            int n = find(vertices[v]);
-            vertices[v] = n;
-            return n;
+        try {
+            if (vertices[v] < 0) {
+                // this node is the root!
+                return v;
+            } else {
+                int n = find(vertices[v]);
+                vertices[v] = n;
+                return n;
+            }
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
         }
     }
 
@@ -69,7 +66,6 @@ public class UnionFind {
        with itself or vertices that are already connected should not change the
        structure. */
     public void union(int v1, int v2) {
-        // TODO: YOUR CODE HERE
         int s1 = sizeOf(v1);
         int s2 = sizeOf(v2);
 
